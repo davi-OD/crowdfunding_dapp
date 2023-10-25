30a54df921668c9c8dd5025938683bf0f7b8b695
davi-OD <davidodie51@gmail.com>
2023-10-25 23:47:30 +0300

    Added the network conf

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    base: {
+      url: process.env.BASE_TESTNET,
+      accounts: process.env.PRIVATE_KEY,
+    }
+  }
 };
